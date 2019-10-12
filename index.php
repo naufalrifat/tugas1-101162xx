747dba992c09e7d175dbbf0ac23be5e752e02186
Farrid Kuntoro <farridkuntoro@email.unikom.ac.id>
2019-10-12 12:02:33 +0700

    menambahkan index.php individu

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
 <body>
   <p>Nama: Naufal Rifat Putrama </p>
   <p>NIM : 10116211</p>
-  <a href="tugas1-master-cc4.herokuapp.com">Kembali ke master</a>
+  <a href="https://tugas1-master-cc4.herokuapp.com/">Kembali ke master</a>
 </body>
 </html>
